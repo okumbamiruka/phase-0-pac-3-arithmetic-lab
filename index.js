1c8a4cb8eecd6067ae7615924e39fbcc7afc6b02
Chrispine Oburu <chrizoburu65@gmail.com>
2024-03-11 16:33:00 +0300

    arithmetic functions

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+    return a + b
+}
+
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function mod(a, b) {
+    return a % b
+}
+
+function increment(n) {
+    return n + 1
+}
+
+function decrement(n) {
+    return n - 1
+}
